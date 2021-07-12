71f31d36c34476af036ee3e21f0c75e742b4db3f
Sankarganesh Gandhi <sankarganesh.gandhi@gmail.com>
2021-07-12 15:44:34 +0200

    mainfests, script for psql and raft were added

--- platform-services/vault-bash-raft.sh
+++ platform-services/vault-bash-raft.sh
@@ -18,4 +18,6 @@
 kubectl -n vault exec vault-0 -- vault operator init -key-shares=1 -key-threshold=1 -format=json > vault-cluster-keys.json
 VAULT_UNSEAL_KEY=$(cat vault-cluster-keys.json | jq -r ".unseal_keys_b64[]")
 kubectl -n vault exec vault-0 -- vault operator unseal $VAULT_UNSEAL_KEY
+
+kubectl -n vault exec -ti vault-1 -- vault operator raft join http://vault-0.vault-internal:8200
 kubectl -n vault exec vault-1 -- vault operator unseal $VAULT_UNSEAL_KEY
