07085f2f788777ab2fb59ac133bd7d1d4a4b56a7
Sankarganesh Gandhi <sankarganesh.gandhi@gmail.com>
2021-08-09 17:10:23 +0200

    secrets from vault delivered syncing with k8s secrets using external secrets

--- myservice1/src/main/java/org/acme/rest/EnvironmentResource.java
+++ myservice1/src/main/java/org/acme/rest/EnvironmentResource.java
@@ -7,19 +7,27 @@ import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+
 @Path("/envvariables")
 public class EnvironmentResource {
+    private final static String SECRET_DB_USERNAME = "username";
+    private final static String SECRET_DB_PASSWORD = "password";
 
     @ConfigProperty(name="svc.database.url", defaultValue="not set")
     String databaseURL;
 
-    @ConfigProperty(name="username", defaultValue="not set")
-    String userNameFromEnv;
+    @ConfigProperty(name=SECRET_DB_USERNAME, defaultValue="not set")
+    String dbUserNameSecret;
+
+    @ConfigProperty(name=SECRET_DB_PASSWORD, defaultValue="not set")
+    String dbPasswordSecret;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getEnvVariables() {
-        String envVariables = "Database URL: " + databaseURL + ";userName: " + userNameFromEnv;
-        return "Env Variables: " + envVariables;
+        String envVariables = "Database URL: " + databaseURL + 
+                                ";dbUserName: " + dbUserNameSecret +
+                                ";dbPassword: " + dbPasswordSecret;
+        return "Environment Variables: " + envVariables;
     }
 }
